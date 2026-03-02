42e8a0c548437ad1473e6494e9b571e7124e4915
Theo Soumarmon <Soumarmon.theo@gmail.com>
2026-03-02 14:34:39 +0100

    patch

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,60 +1,56 @@
 #include <iostream>
-#include <random>
-/*
-* Jeu du nombre mystère
-➤ Le programme choisit un nombre aléatoire entre 1 et 100.
-➤ L’utilisateur doit deviner le nombre.
-➤ Le programme indique “trop grand” ou “trop petit”.
+#include <cstdlib>
+#include <ctime>
 
-
-
-
-
-
-
-*/
 int main()
 {
-	std::srand(std::time(0)); 
-	int min = 0;
-	int max = 100;
-	int vie = 5;
-	int nombre;
+    std::srand(std::time(0));
 
-	int randomNumber = min + rand() % (max - min + 1);
+    int min = 0;
+    int max = 100;
+    int vie = 5;
+    int nombre;
 
-	bool gameend = false;
-	std::cout << "Choissez un nombre entre  0 et 100" << std::endl;
-	
-	std::cout << randomNumber << std::endl;
+    int randomNumber = min + rand() % (max - min + 1);
 
+    bool gameend = false;
 
-	while (!gameend)
-	{
-		std::cin >> nombre;
-		
-		if (nombre > min || nombre < max  )
-		{
-			std::cout << "nombre invalide" << std::endl;
-			continue;
-		}
-		if (nombre == randomNumber)
-		{
-			std::cout << "Félicitation tu as trouvé le nombre " << std::endl;
-			gameend = true;
-		}
-		else {
-			vie -= 1;
-			if (randomNumber < nombre && randomNumber != nombre)
-				std::cout << "le nombre est plus petit " << std::endl;
-			else if (randomNumber > nombre && randomNumber != nombre)
-				std::cout << "le nombre est plus grand" << std::endl;
-			std::cout << "faux il vous reste " << vie << std::endl;
-		}
+    std::cout << "Choisissez un nombre entre 0 et 100" << std::endl;
 
+    while (!gameend)
+    {
+        std::cin >> nombre;
 
-		if (vie == 0)
-			std::cout << "perdu le nombre etait : " << randomNumber << std::endl;
-	}
-	return 0;
+     
+        if (nombre < min || nombre > max)
+        {
+            std::cout << "Nombre invalide" << std::endl;
+            continue;
+        }
+
+        if (nombre == randomNumber)
+        {
+            std::cout << "Félicitations, tu as trouvé le nombre !" << std::endl;
+            gameend = true;
+        }
+        else
+        {
+            vie--;
+
+            if (randomNumber < nombre)
+                std::cout << "Le nombre est plus petit" << std::endl;
+            else
+                std::cout << "Le nombre est plus grand" << std::endl;
+
+            std::cout << "Faux, il vous reste " << vie << " vies" << std::endl;
+
+            if (vie == 0)
+            {
+                std::cout << "Perdu, le nombre était : " << randomNumber << std::endl;
+                gameend = true;
+            }
+        }
+    }
+
+    return 0;
 }
